6af4b8ce04eafc148f9b5998f7d4081d3ad515d7
Hasyirin Fakhriy <ahrim45@gmail.com>
2025-03-13 16:55:21 +0800

    update service provider

--- src/KPIServiceProvider.php
+++ src/KPIServiceProvider.php
@@ -9,17 +9,12 @@ class KPIServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-kpi')
             ->hasConfigFile()
-            ->hasViews()
             ->hasMigrations([
-                'create_laravel_kpi_table',
+                'create_movements_table',
+                'create_holidays_table',
             ]);
     }
 }
