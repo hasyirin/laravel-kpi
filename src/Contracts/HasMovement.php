03b948406b186963b055d56d37e6ed15131e2928
Hasyirin Fakhriy <ahrim45@gmail.com>
2025-03-13 18:09:15 +0800

    update signature

--- src/Contracts/HasMovement.php
+++ src/Contracts/HasMovement.php
@@ -2,10 +2,13 @@
 
 namespace Hasyirin\KPI\Contracts;
 
+use BackedEnum;
 use Hasyirin\KPI\Models\Movement;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Carbon;
 
 /**
  * @property ?Movement $movement
@@ -17,5 +20,5 @@ interface HasMovement
 
     public function movements(): MorphMany;
 
-    public function pass(): Movement;
+    public function pass(BackedEnum|string $status, User|int|null $sender = null, User|int|null $actor = null, ?Carbon $receivedAt = null, ?string $notes = null, Collection|array|null $properties = null): Movement;
 }
